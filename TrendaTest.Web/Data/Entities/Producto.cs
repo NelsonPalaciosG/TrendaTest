e20fffc625bb1d8e5b1445601efa581a43fb334a
NELSON ALEJANDRO PALACIOS GALEANO <nelsonpalacios98055@correo.itm.edu.co>
2020-05-20 21:16:53 -0500

    Add Crud to tienda and add product entitie

---
+++ TrendaTest.Web/Data/Entities/Producto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrendaTest.Web.Data.Entities
+{
+    public class Producto
+    {
+
+        [Key]
+        [Display(Name = "SKU")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public int Sku { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [MaxLength(50, ErrorMessage = "El {0} campo no puede tener mas de {1} caracteres.")]
+        public string Nombre { get; set; }
+
+        [Display(Name = "Fecha de apertura")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [DataType(DataType.DateTime)]//modificar como se recibe la fecha a dd-mm-YYYY
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd hh:mm}", ApplyFormatInEditMode = false)]
+        public DateTime FechaApertura { get; set; }
+
+        [Display(Name = "Descripción")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [MaxLength(50, ErrorMessage = "El {0} campo no puede tener mas de {1} caracteres.")]
+        public string Descripcion { get; set; }
+
+        [RegularExpression("(^[0-9]+$)", ErrorMessage = "Solo se permiten números")]
+        [MaxLength(15, ErrorMessage = "El {0} campo no puede tener mas de {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public int Valor { get; set; }
+
+        [Display(Name = "Imagen")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string PicturePath { get; set; }
+
+        public Tienda Tienda { get; set; }
+    }
+}
