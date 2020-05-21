41b7420622736b3f67652abd6032391009e54a74
NELSON ALEJANDRO PALACIOS GALEANO <nelsonpalacios98055@correo.itm.edu.co>
2020-05-20 21:00:09 -0500

    Add tienda entity and config datacontext to create and migrate db

---
+++ TrendaTest.Web/Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TrendaTest.Web.Data.Entities;
+
+namespace TrendaTest.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Tienda> Tienda { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
---
+++ TrendaTest.Web/Data/Entities/Tienda.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrendaTest.Web.Data.Entities
+{
+    public class Tienda
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [MaxLength(50, ErrorMessage = "El {0} campo no puede tener mas de {1} caracteres.")]
+        public string Nombre { get; set; }
+
+    }
+}
